29918ce10f9f2abdeccec65b8b81f72b54f3b469
Abdelkader  Aoutem <abdlkadraoutem@gmail.com>
2024-03-18 02:10:29 +0000

    in progress

--- C08/ex01/Span.cpp
+++ C08/ex01/Span.cpp
@@ -6,17 +6,34 @@
 /*   By: aaoutem- <aaoutem-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 07:09:41 by aaoutem-          #+#    #+#             */
-/*   Updated: 2024/03/17 20:59:54 by aaoutem-         ###   ########.fr       */
+/*   Updated: 2024/03/17 21:55:42 by aaoutem-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-Span::Span()
+Span::Span() : N(10)
+{}
+Span::Span(int maxNbrs) : N(maxNbrs)
 {}
 Span::Span(const Span& other)
 {}
+
 Span& Span::operator=(const Span& other)
 {}
+void    Span::addNumber(int nbr)
+{
+	if (span.size() == N)
+		throw FullContainer();// throw an exception
+	span.push_back(nbr);
+}
+int	Span::shortestSpan()
+{
+	
+}
+int	Span::longestSpan()
+{
+	
+}
 Span::~Span()
 {}
--- C08/ex01/Span.hpp
+++ C08/ex01/Span.hpp
@@ -6,31 +6,42 @@
 /*   By: aaoutem- <aaoutem-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 06:43:27 by aaoutem-          #+#    #+#             */
-/*   Updated: 2024/03/17 07:14:01 by aaoutem-         ###   ########.fr       */
+/*   Updated: 2024/03/18 01:48:35 by aaoutem-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <exception>
 #include <iostream>
 #include <vector>
+
+typedef	std::vector<int>	IntVect;
 
 class Span
 {
 private :
-	unsigned int		N;
-	std::vector<int>	vect;
+	IntVect			span;
+	unsigned int	N;
 public :
+	class FullContainer : public std::exception 
+	{
+	public :
+		const char* what() const _NOEXCEPT
+		{
+			return "the Container is Full, no place left";
+		}
+	};
 	Span();
+	Span(int maxNbrs);
 	Span(const Span& other);
 	Span& operator=(const Span& other);
 
-	void    addNumber()
-	{}
+	void    addNumber( int nbr );
 
+	int	shortestSpan();
+	int	longestSpan();
 	~Span();
 };
 
