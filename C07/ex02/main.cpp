8839072169eb3f7f0ea140d697d6417126a3f5cc
Kadigh53 <abdlkadraoutem@gmail.com>
2024-11-16 11:16:47 +0100

    c07 nadi

--- C07/ex02/main.cpp
+++ C07/ex02/main.cpp
@@ -6,41 +6,32 @@
 /*   By: aaoutem- <aaoutem-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 01:06:18 by aaoutem-          #+#    #+#             */
-/*   Updated: 2024/11/16 10:59:02 by aaoutem-         ###   ########.fr       */
+/*   Updated: 2024/11/16 11:14:32 by aaoutem-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
 #define MAX_VAL 69
-void a()
-{
-	system("leaks main");
-}
 int main(int, char**)
 {
-	atexit(a);
-    Array<int>numbers(MAX_VAL);
-
+    Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
-
     for (int i = 0; i < MAX_VAL; i++)
     {
-        const int value = i;
+        int value = rand();
         numbers[i] = value;
         mirror[i] = value;
     }
-	for (int i = 0; i < MAX_VAL; i++)
-    {
-		std::cout << numbers[i] << " ";
-	}
-	std::cout << std::endl;
+    //SCOPE
     {
         Array<int> tmp = numbers;
         Array<int> test(tmp);
     }
-
+	for (int i=0;i<MAX_VAL;i++)
+		std::cout << numbers[i] << " ";
+	std::cout<< std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
         if (mirror[i] != numbers[i])
@@ -65,11 +56,13 @@ int main(int, char**)
     {
         std::cerr << e.what() << '\n';
     }
-
+	std::cout<< std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
         numbers[i] = rand();
+		std::cout << numbers[i] << " " ;
     }
-    delete [] mirror;
+	std::cout<< std::endl;
+    delete [] mirror;//
     return 0;
 }
